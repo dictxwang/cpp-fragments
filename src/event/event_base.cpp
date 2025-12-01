dc47a84006fdf2e2d49215184e9a440e44a2fcdf
dictwang <dictwang@163.com>
2025-12-01 16:05:21 +0800

    add event samples

--- src/event/event_base.cpp
+++ src/event/event_base.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <event.h>
 #include <signal.h>
+#include <thread>
+#include <chrono>
+#include <queue>
+#include <mutex>
+#include <shared_mutex>
 
 // Signal handler callback
 void signal_cb(evutil_socket_t sig, short events, void *user_data) {
@@ -48,7 +53,9 @@ void signal_event_base() {
 
 // Timer handler callback
 void timer_cb(evutil_socket_t fd, short events, void *user_data) {
-    std::cout << "Timer event triggered.\n";
+    int *times = (int*)user_data;
+    std::this_thread::sleep_for(std::chrono::seconds(*times));
+    std::cout << "Timer event triggered for " << *times << "\n";
 }
 
 void timer_event_base() {
@@ -62,8 +69,9 @@ void timer_event_base() {
     
     // Create a timer event
     struct timeval five_second = {5, 0}; // 5 second
+    int times = 5;
     // struct event *timer_event = event_new(base, -1, EV_PERSIST, timer_cb, nullptr); // EV_PERSIST makes it repeat
-    struct event *timer_event = evtimer_new(base, timer_cb, nullptr); // w rapper about event_new(), will not repeat
+    struct event *timer_event = evtimer_new(base, timer_cb, &times); // w rapper about event_new(), will not repeat
     event_add(timer_event, &five_second);
 
     // Start ebent loop
@@ -94,9 +102,15 @@ void multi_event_base() {
     event_add(sigint_event, nullptr);
 
     // Create a timer event
-    struct timeval three_second = {3, 0}; // 5 second
-    struct event *timer_event = event_new(base, -1, EV_PERSIST, timer_cb, nullptr);
+    struct timeval three_second = {3, 0}; // 3 second
+    int times_three = 3;
+    struct event *timer_event = event_new(base, -1, EV_PERSIST, timer_cb, &times_three);
     event_add(timer_event, &three_second);
+
+    struct timeval two_second = {2, 0}; // 2 second
+    int times_two = 2;
+    struct event *timer_event_another = event_new(base, -1, EV_PERSIST, timer_cb, &times_two);
+    event_add(timer_event_another, &two_second);
 
     std::cout << "Event loop running. Press Ctrl+C to exit.\n";
 
@@ -111,11 +125,119 @@ void multi_event_base() {
     }
 }
 
+void user_callback(evutil_socket_t fd, short what, void* arg) {
+    std::cout << "User event triggered!\n";
+    // what will be EV_READ when triggered
+}
+
+void user_event_base() {
+    
+    event_base* base = event_base_new();
+
+    // Create a manual event (no fd, no timeout)
+    event* user_event = event_new(base, -1, EV_PERSIST, user_callback, nullptr);
+    event_add(user_event, nullptr);
+
+    // Trigger from another thread
+    std::thread t([base, user_event]() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // Thread-safe way to trigger event
+        event_active(user_event, EV_READ, 0);
+
+        // Or use event_base_once for thread safety
+        struct timeval tv = {0, 0};
+        event_base_once(base, -1, EV_TIMEOUT,
+                        [](evutil_socket_t, short, void*) {
+                            std::cout << "Triggered via event_base_once\n";
+                        }, nullptr, &tv);
+    });
+
+    // Run for 2 seconds
+    struct timeval tv = {2, 0};
+    event_base_loopexit(base, &tv);
+    event_base_dispatch(base);
+
+    t.join();
+    // std::this_thread::sleep_for(std::chrono::seconds(5));
+    event_free(user_event);
+    event_base_free(base);
+}
+
+struct EvWorkItem {
+    std::function<void()> task;
+};
+
+class EventLoopWorker {
+    event_base* base;
+    event* work_event;
+    std::queue<EvWorkItem> work_queue;
+    std::mutex queue_mutex;
+
+    static void work_callback(evutil_socket_t fd, short what, void *arg) {
+        std::cout << "004" << std::endl;
+        auto* worker = static_cast<EventLoopWorker*>(arg);
+        std::cout << "005" << std::endl;
+        worker->processWork();
+    }
+    public:
+      EventLoopWorker() {
+          base = event_base_new();
+          work_event = evuser_new(base, work_callback, this);
+            //   event_add(work_event, nullptr);
+        if (work_event == nullptr || event_add(work_event, nullptr) < 0) {
+            std::cout << "Could not create/add SIGINT event.\n";
+            return;
+        }
+        event_add(work_event, nullptr);
+      }
+      void submitWork(std::function<void()> task) {
+        {
+            std::lock_guard<std::mutex> lock(queue_mutex);
+            work_queue.push({task});
+        }
+        if (work_event == nullptr) {
+            std::cout << "Could not create/add SIGINT event.\n";
+            return;
+        }
+        evuser_trigger(work_event);
+      }
+      void processWork() {
+          std::vector<EvWorkItem> items;
+          {
+              std::lock_guard<std::mutex> lock(queue_mutex);
+              while (!work_queue.empty()) {
+                  items.push_back(work_queue.front());
+                  work_queue.pop();
+              }
+          }
+          std::cout << "005" << std::endl;
+
+          for (auto& item : items) {
+              item.task();
+          }
+      }
+      void run() {
+          event_base_dispatch(base);
+      }
+
+      void stop() {
+          event_base_loopbreak(base);
+      }
+};
+
 int main() {
 
     // signal_event_base();
     // timer_event_base();
+    // multi_event_base();
+    // user_event_base();
+    
+    EventLoopWorker worker;
+    std::thread worker_thread([&worker]() { worker.run(); });
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    multi_event_base();
+    worker.submitWork([]() { std::cout << "Task 1\n"; });
+    worker.submitWork([]() { std::cout << "Task 2\n"; });
     return 0;
 }
